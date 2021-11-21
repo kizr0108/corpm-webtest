0e5a53730dba3e173c10f69e7b78abedb930a93f
kizr0108 <65641277+kizr0108@users.noreply.github.com>
2021-11-22 08:21:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-﻿<a href="Msite ver2/index.html">会社サイト</a>
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background:#eee;">
+    <div style="background:#fff;border:1px solid #ddd;margin:20px;padding:20px;">
+    <a href="Msite ver2/index.html">会社サイト</a>
+    <a href="MtoBサイト/index.html">会社サイト</a>
+    <a href="MtoCサイト/index.html">会社サイト</a>
+    </div>
+</body>
+</html>
