664aa9f1fd6758caab337e8fc019a0480f6b57be
kizr0108 <65641277+kizr0108@users.noreply.github.com>
2021-11-22 08:28:29 +0900

    style added

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body style="background:#eee;">
     <div style="background:#fff;border:1px solid #ddd;margin:20px;padding:20px;">
     <a href="Msite ver2/index.html">会社サイト</a>
-    <a href="MtoBサイト/index.html">会社サイト</a>
-    <a href="MtoCサイト/index.html">会社サイト</a>
+    <a href="MtoBサイト/index.html">MtoBサイト</a>
+    <a href="MtoCサイト/index.html">MtoCサイト</a>
     </div>
 </body>
 </html>
