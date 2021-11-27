00659b90def8819896cb80d80d0b2060102d9f44
kizr0108 <65641277+kizr0108@users.noreply.github.com>
2021-11-27 23:03:00 +0900

    ver1.0.1 some changes added

--- Msite ver2/script.js
+++ Msite ver2/script.js
@@ -6,4 +6,46 @@ $(function () {
             $(".header-nav").removeClass("is_open");
         };
     });
+
+
+    /* ----- smooth-scroll ----- */
+    // #で始まるアンカーをクリックした場合に処理
+    $('a[href^="#"]').click(function () {
+        // スクロールの速度
+        var speed = 400; // ミリ秒
+        // アンカーの値取得
+        var href = $(this).attr("href");
+        // 移動先を取得
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        // 移動先を数値で取得
+        var position = target.offset().top - 100;
+        // スムーススクロール
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+
+    var $win = $(window),
+    $scrollTopLink = $('.scrolltop'),
+    WinHeight = $win.height(),
+    Class = 'is_visible';
+
+    $win.on('load scroll', function () {
+        var value = $(this).scrollTop();
+        if (value > WinHeight) {
+            $scrollTopLink.fadeIn(400);
+            //$scrollTopLink.addClass(Class);
+        } else {
+            $scrollTopLink.fadeOut(400);
+            //$scrollTopLink.removeClass(Class);
+        }
+    });
+
+    $scrollTopLink.click(function(){
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400, 'swing');
+        return false;
+    });
 });
